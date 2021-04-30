568ffc6fccf5743d8ad0a2f0db2198af7b68c0af
EduardoArturoEspinozaMenacho <81436499+EduardoArturoEspinozaMenacho@users.noreply.github.com>
2021-04-30 09:31:37 -0500

    Update Cifrado Afín.cpp

--- Cifrado Afín.cpp
+++ Cifrado Afín.cpp
@@ -25,29 +25,29 @@ int euclides(int x1, int x2){
   x2=r;}
  return eu;}
 int euclides_ext(int a, int b) {
-	int r1 = a, r2 = b;
-	int s1 = 1, s2 = 0;
-	int t1 = 0, t2 = 1;
-	int r, s, t;
-	while (r2 > 0) {
-		int q = r1 / r2;
-		r = r1 - (q * r2);
-		r1 = r2;
-		r2 = r;
-		s = s1 - (q * s2);
-		s1 = s2;
-		s2 = s;
-		t = t1 - (q * t2);
-		t1 = t2;
-		t2 = t;
-	}
-	return s1;}
+  int r1=a,r2=b;
+  int s1=1,s2=0;
+  int t1=0,t2=1;
+  int r,s,t;
+  while (r2 > 0) {
+    int q = r1 / r2;
+    r=r1-(q*r2);
+    r1=r2;
+    r2=r;
+    s=s1-(q*s2);
+    s1=s2;
+    s2=s;
+    t=t1-(q*t2);
+    t1=t2;
+    t2=t;
+  }
+  return s1;}
 int inverso_multiplicativo(int a, int b){
-	int inversa = euclides_ext(a,b);
-	if(inversa<0){
-		inversa=modulo(inversa,b);
-	}
-	return inversa;}
+  int inversa = euclides_ext(a,b);
+  if(inversa<0){
+    inversa=modulo(inversa,b);
+  }
+  return inversa;}
 string cifrado_afin(int a, int b, string mensaje){
   string cifrado;
    for(int j=0;j<mensaje.length();j++){
@@ -106,3 +106,5 @@ int main() {
   cout<<decifrado_afin(a,b,mensaje)<<endl;
 }
 }
+
+
